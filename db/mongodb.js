f9bcdcdd555169438550e57a08866256485c4b2f
Senthamiz Kumaran <senthamizh.kumaran@gmail.com>
2018-07-14 12:44:45 +0530

    Heruko Config

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/DeliveryOrder');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/DeliveryOrder');
 
 module.exports.mongoose = mongoose;
